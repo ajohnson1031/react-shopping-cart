780e4c0090b8ebbcb1c3bd59bfbb25218c96b9a5
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-09-18 22:04:33 -0400

    added Cart context provider to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -19,10 +20,12 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
-        {/* Routes */}
-        <Route exact path="/" component={products} />} />
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        <CartContext.Provider value={cart}>
+          <Navigation />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />} />
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
